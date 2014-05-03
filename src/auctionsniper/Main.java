8e49226ea194e5b33a6d259da751321b4bbf11dc
NienKun Yeh <ynk0917@gmail.com>
2014-05-03 23:49:04 +0800

    close connection when window close

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -1,5 +1,8 @@
 package auctionsniper;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.SwingUtilities;
 
 import org.jivesoftware.smack.Chat;
@@ -48,6 +51,7 @@ public class Main {
     }
     
     private void joinAuction(XMPPConnection connection, String itemId) throws XMPPException {
+        disconnectWhenUICloses(connection);
         final Chat chat = connection.getChatManager().createChat(
                 auctionId(itemId, connection),
                 new MessageListener() {
@@ -66,6 +70,16 @@ public class Main {
         this.notToBeGC = chat;
         
         chat.sendMessage(Main.JOIN_COMMAND_FORMAT);
+    }
+    
+    private void disconnectWhenUICloses(final XMPPConnection connection) {
+        ui.addWindowListener(new WindowAdapter() {
+            
+            @Override
+            public void windowClosed(WindowEvent e) {
+                connection.disconnect();
+            }
+        });
     }
     
     private static XMPPConnection connectTo(String hostname, String username, String password) throws XMPPException {
