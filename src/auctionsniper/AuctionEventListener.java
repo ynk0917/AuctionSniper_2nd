16143d8b2d55b02e964e62fac0ab145094f1b3d3
NienKun Yeh <ynk0917@gmail.com>
2014-05-04 13:19:38 +0800

    add enum PriceSource in AuctionEventListener

--- src/auctionsniper/AuctionEventListener.java
+++ src/auctionsniper/AuctionEventListener.java
@@ -1,6 +1,11 @@
 package auctionsniper;
 
 public interface AuctionEventListener {
+    enum PriceSource {
+        FromSniper,
+        FromOtherBidder;
+    }
+    
     public void auctionClosed();
     public void currentPrice(int price, int increment);
 }
