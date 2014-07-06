b3d41fb2e56cb5dd8332a30122a187e2794a767a
NienKun Yeh <ynk0917@gmail.com>
2014-07-06 16:38:38 +0800

    add STATUS_TEXT for lost and won

--- src/auctionsniper/ui/SnipersTableModel.java
+++ src/auctionsniper/ui/SnipersTableModel.java
@@ -10,7 +10,13 @@ public class SnipersTableModel extends AbstractTableModel {
     private static final long serialVersionUID = 6638492513334189284L;
     
     private final static SniperSnapshot STARTING_UP = new SniperSnapshot("", 0, 0, SniperState.JOINING);
-    private static String[] STATUS_TEXT = { MainWindow.STATUS_JOINING, MainWindow.STATUS_BIDDING, MainWindow.STATUS_WINNING };
+    private static String[] STATUS_TEXT = { 
+        MainWindow.STATUS_JOINING,
+        MainWindow.STATUS_BIDDING,
+        MainWindow.STATUS_WINNING,
+        MainWindow.STATUS_LOST,
+        MainWindow.STATUS_WON
+        };
     private String state = MainWindow.STATUS_JOINING;
     private SniperSnapshot snapshot = STARTING_UP;
 
