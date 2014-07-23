d60d6f6946c157568af3d7f250ec6ed2ca5dcd29
NienKun Yeh <ynk0917@gmail.com>
2014-07-23 23:35:13 +0800

    add serial code

--- src/auctionsniper/util/Defect.java
+++ src/auctionsniper/util/Defect.java
@@ -1,21 +1,21 @@
 package auctionsniper.util;
 
 public class Defect extends RuntimeException {
+    private static final long serialVersionUID = 5269052036344976541L;
 
-  public Defect() {
-    super();
-  }
+    public Defect() {
+        super();
+    }
 
-  public Defect(String message, Throwable cause) {
-    super(message, cause);
-  }
+    public Defect(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-  public Defect(String message) {
-    super(message);
-  }
+    public Defect(String message) {
+        super(message);
+    }
 
-  public Defect(Throwable cause) {
-    super(cause);
-  }
-
+    public Defect(Throwable cause) {
+        super(cause);
+    }
 }
