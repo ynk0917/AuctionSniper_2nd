ccd5157f1d01553789830e8c4f9727ca2e0e2388
NienKun Yeh <ynk0917@gmail.com>
2014-07-23 22:57:00 +0800

    add SniperPortfolio for recording sniper activities

---
+++ src/auctionsniper/SniperPortfolio.java
@@ -0,0 +1,27 @@
+package auctionsniper;
+
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.List;
+
+import auctionsniper.util.Announcer;
+
+public class SniperPortfolio implements SniperCollector {
+    public interface PortfolioListener extends EventListener {
+        public void sniperAdded(AuctionSniper sniper);
+    }
+    
+    private final Announcer<PortfolioListener> announcer = Announcer.to(PortfolioListener.class);
+    private final List<AuctionSniper> snipers = new ArrayList<AuctionSniper>();
+
+    @Override
+    public void addSniper(AuctionSniper sniper) {
+        snipers.add(sniper);
+        announcer.announce().sniperAdded(sniper);
+    }
+    
+    public void addPortfolioListener(PortfolioListener listener) {
+        announcer.addListener(listener);
+    }
+
+}
