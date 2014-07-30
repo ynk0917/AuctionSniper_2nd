2001f9a3d1690b08b0d3569d4f815cd04a0f3e15
NienKun Yeh <ynk0917@gmail.com>
2014-07-30 23:36:36 +0800

    add Item class

--- src/auctionsniper/UserRequestListener.java
+++ src/auctionsniper/UserRequestListener.java
@@ -1,7 +1,35 @@
 package auctionsniper;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 import java.util.EventListener;
 
 public interface UserRequestListener extends EventListener {
     void joinAuction(String itemId);
+
+    public static class Item {
+        public final String identifier;
+        public final int stopPrice;
+
+        public Item(String identifier, int stopPrice) {
+            this.identifier = identifier;
+            this.stopPrice = stopPrice;
+        }
+
+        @Override
+        public boolean equals(Object rhs) {
+            return EqualsBuilder.reflectionEquals(this, rhs);
+        }
+
+        @Override
+        public int hashCode() {
+            return HashCodeBuilder.reflectionHashCode(this);
+        }
+
+        @Override
+        public String toString() {
+            return "Item: " + identifier + ", stop price: " + stopPrice;
+        }
+    }
 }
