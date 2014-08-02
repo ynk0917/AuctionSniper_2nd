e9b1bd89bb62645ceff03e5e8df1af8bdc1bb876
NienKun Yeh <ynk0917@gmail.com>
2014-08-02 12:00:41 +0800

    add unittest continuesToBeLosingOnceStopPriceHasBeenReached

--- test/unit/test/auctionsniper/AuctionSniperTest.java
+++ test/unit/test/auctionsniper/AuctionSniperTest.java
@@ -7,6 +7,7 @@ import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.Sequence;
 import org.jmock.States;
 import org.jmock.integration.junit4.JMock;
 import org.junit.Before;
@@ -62,7 +63,20 @@ public class AuctionSniperTest {
         }});
     }
 
+    @Test
+    public void continuesToBeLosingOnceStopPriceHasBeenReached() {
+        final Sequence states = context.sequence("sniper states");
+        final int price1 = 1233;
+        final int price2 = 1258;
 
+        context.checking(new Expectations() {{
+            atLeast(1).of(sniperListener).sniperStateChanged(new SniperSnapshot(ITEM_ID, price1, 0, SniperState.LOSING)); inSequence(states);
+            atLeast(1).of(sniperListener).sniperStateChanged(new SniperSnapshot(ITEM_ID, price2, 0, SniperState.LOSING)); inSequence(states);
+        }});
+
+        sniper.currentPrice(price1, 25, PriceSource.FromOtherBidder);
+        sniper.currentPrice(price2, 25, PriceSource.FromOtherBidder);
+    }
 
     @Test
     public void bidsHigherAndReportsBiddingWhenNewPriceArrives() {
@@ -146,7 +160,7 @@ public class AuctionSniperTest {
             allowing(auction).bid(bid);
             atLeast(1).of(sniperListener).sniperStateChanged(
                     new SniperSnapshot(ITEM_ID, 2345, bid, SniperState.LOSING));
-                        when(sniperState.is("bidding"));
+            when(sniperState.is("bidding"));
         }});
         sniper.currentPrice(123, 45, PriceSource.FromOtherBidder);
         sniper.currentPrice(2345, 25, PriceSource.FromOtherBidder);
