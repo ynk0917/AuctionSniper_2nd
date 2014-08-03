fee4752675c9a0eba88b6effaba1814fe4b6a347
NienKun Yeh <ynk0917@gmail.com>
2014-08-04 00:15:17 +0800

    pass unittest notifiesAuctionFailedWhenEventTypeMissing

--- src/auctionsniper/xmpp/AuctionMessageTranslator.java
+++ src/auctionsniper/xmpp/AuctionMessageTranslator.java
@@ -28,7 +28,7 @@ public class AuctionMessageTranslator implements MessageListener {
         }
     }
 
-    private void translate(String messageBody) {
+    private void translate(String messageBody) throws Exception {
         AuctionEvent event = AuctionEvent.from(messageBody);
 
         String type = event.type();
@@ -41,22 +41,26 @@ public class AuctionMessageTranslator implements MessageListener {
 
     private static class AuctionEvent {
         private final Map<String, String> fields = new HashMap<String, String>();
-        public String type() { return get("Event"); }
-        public int currentPrice() { return getInt("CurrentPrice"); }
-        public int increment() { return getInt("Increment"); }
-        public PriceSource isFrom(String sniperId) {
+        public String type() throws MissingValueException { return get("Event"); }
+        public int currentPrice() throws Exception { return getInt("CurrentPrice"); }
+        public int increment() throws Exception { return getInt("Increment"); }
+        public PriceSource isFrom(String sniperId) throws MissingValueException {
             return sniperId.equals(bidder()) ? PriceSource.FromSniper : PriceSource.FromOtherBidder;
         }
         
-        private String bidder() {
+        private String bidder() throws MissingValueException {
             return get("Bidder");
         }
         
-        private int getInt(String fieldName) {
+        private int getInt(String fieldName) throws MissingValueException {
             return Integer.parseInt(get(fieldName));
         }
         
-        private String get(String fieldName) {
+        private String get(String fieldName) throws  MissingValueException {
+            String value = fields.get(fieldName);
+            if (value == null) {
+                throw new MissingValueException(fieldName);
+            }
             return fields.get(fieldName);
         }
         
@@ -79,4 +83,9 @@ public class AuctionMessageTranslator implements MessageListener {
         
     }
 
+    private static class MissingValueException extends Exception {
+        public MissingValueException(String fieldName) {
+            super("Missing value for " + fieldName);
+        }
+    }
 }
