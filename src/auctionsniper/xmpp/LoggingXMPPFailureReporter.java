6ca06f208eed567d49739d1f1603656bc1756bcb
NienKun Yeh <ynk0917@gmail.com>
2014-08-05 01:33:12 +0800

    pass unittest writesMessageTranslateFailureToLog

--- src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
+++ src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
@@ -2,11 +2,18 @@ package auctionsniper.xmpp;
 
 import java.util.logging.Logger;
 
+import static java.lang.String.format;
+
 public class LoggingXMPPFailureReporter implements XMPPFailureReporter {
+    private static final String MESSAGE_FORMAT = "<%s> Could not translate message \"%s\" because \"%s\"";
+
+    private Logger logger;
     public LoggingXMPPFailureReporter(Logger logger) {
+        this.logger = logger;
     }
 
     @Override
     public void cannotTranslateMessage(String auctionId, String failedMessage, Exception exception) {
+        logger.severe(format(MESSAGE_FORMAT, auctionId, failedMessage, exception.toString()));
     }
 }
